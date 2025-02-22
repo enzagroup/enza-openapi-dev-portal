a3fafd086988ccf404fa5e1badf9b795776089b0
enzagroup <dev@enzagroup.global>
2025-02-22 23:16:50 +0200

    Updates

--- assets/context-h_UkBLvr-DvUA59nw.js
+++
@@ -1,2 +0,0 @@
-import{d as r,O as i}from"./entry.client-Cnp7BFTR.js";const n=r.createContext(void 0),d=({children:e,client:t})=>i.jsx(n.Provider,{value:t,children:e}),o=r.createContext(void 0),a=o.Provider,c=()=>{const e=r.useContext(o);if(!e)throw new Error("useOasConfig must be used within a OasConfigProvider");return e.config};export{c as d,n as i,d as u,a as x};
-//# sourceMappingURL=context-h_UkBLvr-DvUA59nw.js.map
